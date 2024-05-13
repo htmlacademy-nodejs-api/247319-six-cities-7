cae8e331f3c20e5e546c85f2f9b0658647f98290
Roman Milaev <r.milaev@yandex.ru>
2024-05-14 00:36:11 +0300

    создал сущность User для добавления в базу данных

---
+++ src/shared/modules/user/user.entity.ts
@@ -0,0 +1,49 @@
+import { getModelForClass, prop, defaultClasses, modelOptions } from '@typegoose/typegoose';
+import { User } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class UserEntity extends defaultClasses.TimeStamps implements User {
+  @prop({unique: true, required: true})
+  public email: string;
+
+  @prop({required: true})
+  public name: string;
+
+  @prop({required: false, default: ''})
+  public avatarUrl: string;
+
+  @prop({required: true})
+  public isPro: boolean;
+
+  @prop({required: true, default: '' })
+  private password?: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.name = userData.name;
+    this.avatarUrl = userData.avatarUrl;
+    this.isPro = userData.isPro;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
+}
+
+export const UserModel = getModelForClass(UserEntity);
